23f77336943795219230765c7ec82b658eabed48
Zeynep Özdemir Açıkgöz <zeynmirgmail.com>
2022-11-17 15:00:04 +0300

    no message

--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -8,8 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var button1: UIButton!
+    
+    @IBOutlet weak var button2: UIButton!
+    
+    @IBOutlet weak var button3: UIButton!
+    
+    @IBOutlet weak var button4: UIButton!
+    
+    @IBOutlet weak var button4View2: UIButton!
+    
+    @IBOutlet weak var button6view2: UIButton!
+    
+    @IBOutlet weak var button7View2: UIButton!
+    
+    @IBOutlet weak var buttton8View2: UIButton!
+    
+    @IBOutlet weak var button9View3: UIButton!
+    
+    @IBOutlet weak var button10View3: UIButton!
+    
+    @IBOutlet weak var button11View3: UIButton!
+    
+    @IBOutlet weak var button12View3: UIButton!
+    
+    @IBOutlet weak var button13View4: UIButton!
+    
+    @IBOutlet weak var button14View4: UIButton!
+    
+    @IBOutlet weak var button15view4: UIButton!
+    
+    @IBOutlet weak var button16View4: UIButton!
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
